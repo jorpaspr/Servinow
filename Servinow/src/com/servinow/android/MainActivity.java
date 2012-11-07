969798ff9a9ef7157d064e5243bef43b59144cc5
José Cabo <folcwine@gmail.com>
2012-11-07 13:10:22 +0100

    Repo cleaning after destroy it with a gitbomb.

--- Servinow/src/com/servinow/android/MainActivity.java
+++ Servinow/src/com/servinow/android/MainActivity.java
@@ -20,11 +20,6 @@ public class MainActivity extends SherlockActivity {
     	 * The first is the one for the ActionBarSherlock library.
     	 * The second if used for the oficial Android API but the API are ""fully"" compatible.
     	 * */
-    	
-    	//TestCabo
-    	//test blabla
-    	
-    	//Test1 nuevo test y tal y pascual.
     	getSupportMenuInflater().inflate(R.menu.activity_main, menu);/*Al "menú" correspondiente le pones este menú (activity_main)*/
         return true;
     }
