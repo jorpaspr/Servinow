dd90d0e0fd396d128677ae9f174a32bd30f971f6
José Cabo <folcwine@gmail.com>
2012-11-09 18:02:09 +0100

    Just code clean.

--- Servinow/src/com/servinow/android/QRReading.java
+++ Servinow/src/com/servinow/android/QRReading.java
@@ -1,31 +1,19 @@
 package com.servinow.android;
 
-import net.sourceforge.zbar.Config;
-import net.sourceforge.zbar.Image;
-import net.sourceforge.zbar.ImageScanner;
-import net.sourceforge.zbar.Symbol;
-import net.sourceforge.zbar.SymbolSet;
 import android.content.pm.ActivityInfo;
-import android.hardware.Camera;
-import android.hardware.Camera.AutoFocusCallback;
-import android.hardware.Camera.PreviewCallback;
-import android.hardware.Camera.Size;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.actionbarsherlock.app.SherlockActivity;
-import com.servinow.android.QRSystem.CameraPreview;
 import com.servinow.android.QRSystem.QRReadingSystem;
 
 public class QRReading extends SherlockActivity {
-	
+
 	private QRReadingSystem qrReadingSystem;
-	
-    static {
-        System.loadLibrary("iconv");
-    }
+
+	static {
+		System.loadLibrary("iconv");
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -37,13 +25,13 @@ public class QRReading extends SherlockActivity {
 		qrReadingSystem = new QRReadingSystem(this, ((FrameLayout) findViewById(R.id.QRReading_cameraPreview)));
 		//qrReadingSystem.start();
 	}
-	
+
 	@Override
 	protected void onPause() {
 		super.onPause();
 		qrReadingSystem.releaseCamera();
 	}
-	
+
 	@Override
 	protected void onResume() {
 		super.onResume();
