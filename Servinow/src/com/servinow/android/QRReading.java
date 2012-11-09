ac5668931abcf62bbc50c709941aece57405a224
José Cabo <folcwine@gmail.com>
2012-11-09 17:18:38 +0100

    Modularize the QR Reading System to clean the main class code.

--- Servinow/src/com/servinow/android/QRReading.java
+++ Servinow/src/com/servinow/android/QRReading.java
@@ -17,14 +17,11 @@ import android.widget.FrameLayout;
 
 import com.actionbarsherlock.app.SherlockActivity;
 import com.servinow.android.QRSystem.CameraPreview;
+import com.servinow.android.QRSystem.QRReadingSystem;
 
 public class QRReading extends SherlockActivity {
-
-	private Handler autoFocusHandler;
-	private Camera mCamera;
-	private ImageScanner scanner;
-	private CameraPreview mPreview;
-	private boolean previewing = true;
+	
+	private QRReadingSystem qrReadingSystem;
 	
     static {
         System.loadLibrary("iconv");
@@ -37,81 +34,19 @@ public class QRReading extends SherlockActivity {
 
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
 
-		autoFocusHandler = new Handler();
-		mCamera = getCameraInstance();
-
-		/* Instance barcode scanner */
-		scanner = new ImageScanner();
-		scanner.setConfig(0, Config.X_DENSITY, 3);
-		scanner.setConfig(0, Config.Y_DENSITY, 3);
-
-		mPreview = new CameraPreview(this, mCamera, previewCb, autoFocusCB);
-		FrameLayout preview = (FrameLayout)findViewById(R.id.QRReading_cameraPreview);
-		preview.addView(mPreview);
+		qrReadingSystem = new QRReadingSystem(this, ((FrameLayout) findViewById(R.id.QRReading_cameraPreview)));
+		//qrReadingSystem.start();
 	}
-
+	
 	@Override
 	protected void onPause() {
 		super.onPause();
-		releaseCamera();
+		qrReadingSystem.releaseCamera();
 	}
-
-	private void releaseCamera() {
-		if (mCamera != null) {
-			previewing = false;
-			mCamera.setPreviewCallback(null);
-			mCamera.release();
-			mCamera = null;
-		}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		qrReadingSystem.start();
 	}
-
-	private Runnable doAutoFocus = new Runnable() {
-		public void run() {
-			if (previewing)
-				mCamera.autoFocus(autoFocusCB);
-		}
-	};
-
-
-	PreviewCallback previewCb = new PreviewCallback() {
-		public void onPreviewFrame(byte[] data, Camera camera) {
-			Camera.Parameters parameters = camera.getParameters();
-			Size size = parameters.getPreviewSize();
-
-			Image barcode = new Image(size.width, size.height, "Y800");
-			barcode.setData(data);
-
-			int result = scanner.scanImage(barcode);
-
-			if (result != 0) {
-				previewing = false;
-				mCamera.setPreviewCallback(null);
-				mCamera.stopPreview();
-
-				SymbolSet syms = scanner.getResults();
-				for (Symbol sym : syms) {
-					//scanText.setText("barcode result " + sym.getData());
-					Log.i("QR", "Data:_"+sym.getData()+"_fin");
-				}
-			}
-		}
-	};
-
-	// Mimic continuous auto-focusing
-	AutoFocusCallback autoFocusCB = new AutoFocusCallback() {
-		public void onAutoFocus(boolean success, Camera camera) {
-			autoFocusHandler.postDelayed(doAutoFocus, 1000);
-		}
-	};
-
-	/** A safe way to get an instance of the Camera object. */
-	public static Camera getCameraInstance(){
-		Camera c = null;
-		try {
-			c = Camera.open();
-		} catch (Exception e){
-		}
-		return c;
-	}
-
 }
--- Servinow/src/com/servinow/android/QRSystem/QRReadingSystem.java
+++ Servinow/src/com/servinow/android/QRSystem/QRReadingSystem.java
@@ -1,10 +1,103 @@
 package com.servinow.android.QRSystem;
 
-import com.servinow.android.QRReading;
+import net.sourceforge.zbar.Config;
+import net.sourceforge.zbar.Image;
+import net.sourceforge.zbar.ImageScanner;
+import net.sourceforge.zbar.Symbol;
+import net.sourceforge.zbar.SymbolSet;
+import android.content.Context;
+import android.hardware.Camera;
+import android.hardware.Camera.AutoFocusCallback;
+import android.hardware.Camera.PreviewCallback;
+import android.hardware.Camera.Size;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.FrameLayout;
 
 public class QRReadingSystem {
 
-	public QRReadingSystem(QRReading readingActivity) {
-		
+	private Handler autoFocusHandler;
+	private Camera mCamera;
+	private ImageScanner scanner;
+	protected boolean previewing = true;
+	private CameraPreview mPreview;
+
+	private Context context;
+	private FrameLayout container;
+
+	public QRReadingSystem(Context context, FrameLayout container) {
+		this.context = context;
+		this.container = container;
 	}
+
+	public void start(){
+		autoFocusHandler = new Handler();
+		mCamera = getCameraInstance();
+
+		/* Instance barcode scanner */
+		scanner = new ImageScanner();
+		scanner.setConfig(0, Config.X_DENSITY, 3);
+		scanner.setConfig(0, Config.Y_DENSITY, 3);
+
+		mPreview = new CameraPreview(context, mCamera, previewCb, autoFocusCB);
+		container.addView(mPreview);
+	}
+
+	public void releaseCamera() {
+		if (mCamera != null) {
+			previewing = false;
+			mCamera.setPreviewCallback(null);
+			mCamera.release();
+			mCamera = null;
+			container.removeAllViews();
+		}
+	}
+
+	/** A safe way to get an instance of the Camera object. */
+	private static Camera getCameraInstance(){
+		Camera c = null;
+		try {
+			c = Camera.open();
+		} catch (Exception e){
+		}
+		return c;
+	}
+
+	private PreviewCallback previewCb = new PreviewCallback() {
+		public void onPreviewFrame(byte[] data, Camera camera) {
+			Camera.Parameters parameters = camera.getParameters();
+			Size size = parameters.getPreviewSize();
+
+			Image barcode = new Image(size.width, size.height, "Y800");
+			barcode.setData(data);
+
+			int result = scanner.scanImage(barcode);
+
+			if (result != 0) {
+				previewing  = false;
+				mCamera.setPreviewCallback(null);
+				mCamera.stopPreview();
+
+				SymbolSet syms = scanner.getResults();
+				for (Symbol sym : syms) {
+					//scanText.setText("barcode result " + sym.getData());
+					Log.i("QR", "Data:_"+sym.getData()+"_fin");
+				}
+			}
+		}
+	};
+
+	// Mimic continuous auto-focusing
+	private AutoFocusCallback autoFocusCB = new AutoFocusCallback() {
+		public void onAutoFocus(boolean success, Camera camera) {
+			autoFocusHandler.postDelayed(doAutoFocus, 1000);
+		}
+	};
+
+	private Runnable doAutoFocus = new Runnable() {
+		public void run() {
+			if (previewing)
+				mCamera.autoFocus(autoFocusCB);
+		}
+	};
 }
