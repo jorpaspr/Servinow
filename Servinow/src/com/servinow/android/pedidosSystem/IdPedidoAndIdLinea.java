f20903f7fcb40b07d27c2962f7f2173525e0ccf7
Carlos <escrichov@gmail.com>
2012-11-14 00:14:59 +0100

    Modificado idProducto por idPedido

--- Servinow/src/com/servinow/android/pedidosSystem/IdPedidoAndIdLinea.java
+++ Servinow/src/com/servinow/android/pedidosSystem/IdPedidoAndIdLinea.java
@@ -2,7 +2,7 @@ package com.servinow.android.pedidosSystem;
 
 public class IdPedidoAndIdLinea {
 	
-	public int idProducto;
+	public int idPedido;
 	public int idLinea;
 	
 	public IdPedidoAndIdLinea(){
