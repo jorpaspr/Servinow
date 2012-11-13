25e9a3be358c6205ac9fd000d55921442fe9fc5f
Carlos <escrichov@gmail.com>
2012-11-13 21:07:53 +0100

    Eliminada la línea: 	Log.i("Database", "hola");

--- Servinow/src/com/servinow/android/ListaPedidoActivity.java
+++ Servinow/src/com/servinow/android/ListaPedidoActivity.java
@@ -37,8 +37,6 @@ public class ListaPedidoActivity extends SherlockListActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.lista_pedido);
 		
-		Log.i("Database", "hola");
-		
 		// Leer de la base de datos
 		
 		PedidoCache pedidoDAO = new PedidoCache( ListaPedidoActivity.this );
