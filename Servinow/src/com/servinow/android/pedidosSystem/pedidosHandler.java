9cbeaa88108026734fae11ba4f88decdd00512f0
Carlos <escrichov@gmail.com>
2012-11-14 00:34:13 +0100

    Implementada la función insertarPedido(Producto producto, int cantidad, int mesaId, int restauranteId)

--- Servinow/src/com/servinow/android/pedidosSystem/pedidosHandler.java
+++ Servinow/src/com/servinow/android/pedidosSystem/pedidosHandler.java
@@ -1,22 +1,52 @@
 package com.servinow.android.pedidosSystem;
 
+import java.util.Date;
+
+import android.content.Context;
+
+import com.servinow.android.domain.Estado;
+import com.servinow.android.domain.LineaPedido;
+import com.servinow.android.domain.Pedido;
 import com.servinow.android.domain.Place;
 import com.servinow.android.domain.Producto;
 import com.servinow.android.domain.Restaurant;
 import com.servinow.android.pedidosSystem.IdPedidoAndIdLinea;
+import com.servinow.android.dao.LineaPedidoCache;
+import com.servinow.android.dao.PedidoCache;
 
 public class pedidosHandler {
 	
-	public pedidosHandler(){
-		
+	private Context context;
+	
+	public pedidosHandler(Context context){
+		this.context = context;
 	}
 	
-	// TODO Implementar insertarPedido
-	// Tiene que crear un pedido cuando no esté creado.
-	// Crear una línea de pedido
-	// Añadir la línea de pedido al pedido
-	// Devolver el identificador de la línea de pedido creada y el identificador del pedido
-	public IdPedidoAndIdLinea insertarPedido(int pedidoId, int cantidad, int mesaId, int restauranteId){
+	public IdPedidoAndIdLinea insertarPedido(Producto producto, int cantidad, int mesaId, int restauranteId){
+		
+		PedidoCache pedidoCache = new PedidoCache(this.context);
+		LineaPedidoCache lineaPedidoCache = new LineaPedidoCache(this.context);
+		
+		// Si el pedido no está creado, se crea un nuevo pedido no confirmado
+		if(pedidoCache.IsThereOrderNotConfirmed()){
+			Pedido pedido = new Pedido(new Date());
+			pedidoCache.insertPedido(pedido);
+		}
+		Pedido pedidoNoConfirmado = pedidoCache.getPedidoNotConfirmed();
+		
+		// Crear LineaPedido a partir de la cantidad y el producto
+		LineaPedido lineaPedido = new LineaPedido(cantidad, Estado.EN_COLA, producto);
+		lineaPedido.setOrder(pedidoNoConfirmado);
+		
+		// Insertar LineaPedido en la base de datos
+		lineaPedidoCache.insertLineaPedido(lineaPedido);
+		
+		// Devolver el identificador de la línea de pedido creada y el identificador del pedido
+		IdPedidoAndIdLinea idPedidoAndIdLinea = new IdPedidoAndIdLinea();
+		idPedidoAndIdLinea.idPedido = pedidoNoConfirmado.getId();
+		idPedidoAndIdLinea.idLinea = lineaPedido.getId();
+		
+		return idPedidoAndIdLinea;
 	}
 	// TODO Implementar cancelarPedido
 	// Borrar todas las líneas de pedido de ese pedido.
