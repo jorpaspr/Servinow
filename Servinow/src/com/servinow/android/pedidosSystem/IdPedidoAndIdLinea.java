59e5e5b5ed1f06334e859e05f8a30346683778da
Carlos <escrichov@gmail.com>
2012-11-13 23:32:38 +0100

    Añadido pedidosSystem para crear una api de pedidos

---
+++ Servinow/src/com/servinow/android/pedidosSystem/IdPedidoAndIdLinea.java
@@ -0,0 +1,16 @@
+package com.servinow.android.pedidosSystem;
+
+public class IdPedidoAndIdLinea {
+	
+	public int idProducto;
+	public int idLinea;
+	
+	public IdPedidoAndIdLinea(){
+		
+	}
+	
+	public IdPedidoAndIdLinea(int idProducto, int idLinea){
+		this.idProducto = idProducto;
+		this.idLinea = idLinea;
+	}
+}
---
+++ Servinow/src/com/servinow/android/pedidosSystem/pedidosHandler.java
@@ -0,0 +1,27 @@
+package com.servinow.android.pedidosSystem;
+
+import com.servinow.android.domain.Place;
+import com.servinow.android.domain.Producto;
+import com.servinow.android.domain.Restaurant;
+import com.servinow.android.pedidosSystem.IdPedidoAndIdLinea;
+
+public class pedidosHandler {
+	
+	public pedidosHandler(){
+		
+	}
+	
+	// TODO Implementar insertarPedido
+	// Tiene que crear un pedido cuando no esté creado.
+	// Crear una línea de pedido
+	// Añadir la línea de pedido al pedido
+	// Devolver el identificador de la línea de pedido creada y el identificador del pedido
+	public IdPedidoAndIdLinea insertarPedido(int pedidoId, int cantidad, int mesaId, int restauranteId){
+	}
+	// TODO Implementar cancelarPedido
+	// Borrar todas las líneas de pedido de ese pedido.
+	// Borrar Pedido.
+	public void cancelarPedido(){
+		
+	}
+}
