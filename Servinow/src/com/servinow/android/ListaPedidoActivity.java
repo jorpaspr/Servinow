9f2c86c74df41526c799250b1dbfa8f0c78eb276
Carlos <escrichov@gmail.com>
2012-11-15 19:10:47 +0100

    Añadido botón en el menú para que vaya a CategoriasActivity.

--- Servinow/src/com/servinow/android/ListaPedidoActivity.java
+++ Servinow/src/com/servinow/android/ListaPedidoActivity.java
@@ -173,6 +173,16 @@ public class ListaPedidoActivity extends SherlockListActivity {
 	}
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.lista_pedido_button_go_to_categorias:
+    			Intent i = new Intent(ListaPedidoActivity.this, CategoriasActivity.class);
+    			Bundle b = new Bundle();
+    			b.putInt(Param.RESTAURANT.toString(), restaurantID);
+    			b.putInt(Param.PLACE.toString(), placeID);
+    			i.putExtras(b);
+    			startActivity(i);
+        }
 	    return super.onOptionsItemSelected(item);
 	}
 	
