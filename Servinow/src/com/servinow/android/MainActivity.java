2f02b9b53770a44c6da61e97f856a515ed727097
José Cabo <folcwine@gmail.com>
2012-11-15 22:16:35 +0100

    Changed the main activity to the design of Amalio.

--- Servinow/src/com/servinow/android/MainActivity.java
+++ Servinow/src/com/servinow/android/MainActivity.java
@@ -5,37 +5,41 @@ import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import com.actionbarsherlock.app.SherlockActivity;
 
 public class MainActivity extends SherlockActivity {	
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.mainactivity); /*A esta actividad/ventana le pones este layout*/
-        
-        Button qrReadingButton = (Button) findViewById(R.id.mainactivity_qrreading);
-        qrReadingButton.setOnClickListener(new OnClickListener() {
-			
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.mainactivity); /*A esta actividad/ventana le pones este layout*/
+
+		ImageButton qrReadingButton = (ImageButton) findViewById(R.id.mainactivity_qrreading);
+		qrReadingButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				startActivity(new Intent(MainActivity.this, QRReading.class));
 			}
 		});
-        
-        Button directToRestaurantButton = (Button) findViewById(R.id.mainactivity_directtorestaurant);
-        directToRestaurantButton.setOnClickListener(new OnClickListener() {
-			
+
+		findViewById(R.id.mainactivity_gotomap).setEnabled(false);
+
+
+		Button directToRestaurantButton = (Button) findViewById(R.id.mainactivity_directtorestaurant);
+		directToRestaurantButton.setOnClickListener(new OnClickListener() {
+
 			@Override
 			public void onClick(View v) {
 				Intent i = new Intent(MainActivity.this, QRReading.class);
 				Bundle b = new Bundle();
 				b.putBoolean(QRReading.PARAM.GOTORESTAURANT.toString(), true);
 				i.putExtras(b);
-				
+
 				startActivity(i);
 			}
 		});
-    }
+	}
 }
