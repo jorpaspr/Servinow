248932a4ea04bdaafdcf365c51bcd802215aa224
Rafael Blazquez <github@koxalen.net>
2013-01-05 16:42:27 +0100

    Sincronizacion: confirmar pedido

---
+++ Servinow/src/com/servinow/android/restaurantCacheSyncSystem/CallForConfirmar.java
@@ -0,0 +1,49 @@
+package com.servinow.android.restaurantCacheSyncSystem;
+
+import java.io.IOException;
+
+import com.servinow.android.dao.PedidoCache;
+import com.servinow.android.domain.Pedido;
+import com.servinow.android.synchronization.ServinowApi_Confirmar;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+public class CallForConfirmar {
+
+  private final Context context;
+  private final int     restaurantID;
+  private final Pedido pedido;
+
+  public CallForConfirmar(Context context, Pedido pedido, int restaurantID) {
+    this.context = context;
+    this.pedido = pedido;
+    this.restaurantID = restaurantID;
+  }
+
+  public void start() {
+    new task().execute();
+  }
+
+  private class task extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... params) {
+      ServinowApi_Confirmar confirmar = new ServinowApi_Confirmar(restaurantID, pedido);
+      try {
+        String result = confirmar.call();
+        pedido.setOnlineID(Integer.parseInt(result));
+        new PedidoCache(context).updatePedido(pedido);
+      }
+      catch(IOException e) {
+        Log.e("Error", "Bad call to ServinowApi_Confirmar", e);
+      }
+      catch (NumberFormatException e) {
+        Log.e("Error", "Bad return from ServinowApi_Confirmar", e);
+      }
+      return null;
+    }
+  }
+
+}
---
+++ Servinow/src/com/servinow/android/synchronization/ServinowApi_Confirmar.java
@@ -0,0 +1,49 @@
+package com.servinow.android.synchronization;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.servinow.android.domain.LineaPedido;
+import com.servinow.android.domain.Pedido;
+
+public class ServinowApi_Confirmar extends ServinowApi {
+  //Ugly thing incoming...
+  @SuppressWarnings("unused")
+  private class LineaPedidoBag {
+    public int linea_pedido_id;
+    public int producto_id;
+    public int cantidad;
+    public LineaPedidoBag(LineaPedido lineaPedido) {
+      linea_pedido_id = lineaPedido.getId();
+      producto_id = lineaPedido.getProducto().getId();
+      cantidad = lineaPedido.getCantidad();
+    }
+  }
+  @SuppressWarnings("unused")
+  private class PedidoBag {
+    public int mesa_id;
+    public ArrayList<LineaPedidoBag> lineas_pedido;
+    public PedidoBag(Pedido pedido) {
+      mesa_id = pedido.getPlace().getOnlineID();
+      lineas_pedido = new ArrayList<LineaPedidoBag>();
+      Collection<LineaPedido> lineas = pedido.getLineas();
+      Iterator<LineaPedido> lineasIt = lineas.iterator();
+      while(lineasIt.hasNext()) {
+        lineas_pedido.add(new LineaPedidoBag(lineasIt.next()));
+      }
+    }
+  }
+
+  public ServinowApi_Confirmar(int restaurantID, Pedido pedido) {
+    super("/"+ restaurantID + "/api/confirmar");
+    PedidoBag object = new PedidoBag(pedido);
+    //Debug
+    Log.d("GSON", new GsonBuilder().setPrettyPrinting().create().toJson(object));
+    payload = "DATA="+new Gson().toJson(object);
+  }
+}
